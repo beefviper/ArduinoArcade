8171c43f0abc827536c80698785f62908f0f50c8
beefviper <beef.viper@gmail.com>
2017-01-12 19:39:49 -0500

    Commenting out draw() and logic() for other games, so only one game is running at a time for now

--- Arcade3/Game.cpp
+++ Arcade3/Game.cpp
@@ -17,12 +17,12 @@ Game::~Game() {
 
 void Game::draw(U8G2_ST7920_128X64_1_6800 &screen) {
 	snake.draw(screen);
-	missile.draw(screen);
-	breakout.draw(screen);
+	//missile.draw(screen);
+	//breakout.draw(screen);
 }
 
 void Game::logic(Joystick &joystick, Speaker &speaker) {
 	snake.logic(joystick, speaker);
-	missile.logic(joystick, speaker);
-	breakout.logic(joystick, speaker);
+	//missile.logic(joystick, speaker);
+	//breakout.logic(joystick, speaker);
 }
